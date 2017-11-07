12799b9c26bdd624a1fd6994a691e73e70b92668
Qingtian Zou <qingtian.mill.zou@gmail.com>
2017-11-07 15:24:14 -0500

    Finished confidenceProcessing.

--- output_processing.py
+++ output_processing.py
@@ -24,18 +24,46 @@ def confidenceProcessing():
     global files
     global results
     global scores
-    # TODO: processing rules:
+    # processing rules:
     #       1. If none of the scores of results for a file is greater than 0.6, then all results should present.
     #       2. If there is only one score of a result is greater than 0.6, and all other scores are significantly low (no greater than 0.2), then delete all other results.
     #       3. If there is only one score of a result is greater than 0.6, and not all other scores are significantly low (no greater than 0.2), then save all results whose scores are greater than 0.1.
     #       4. If multiple results are greater than 0.6, then save all results whose scores are greater than 0.4.
+    # Just a reminder: the scores are already sorted from high to low when written into file.
     i=-5
     while True:
-        i+=5
-        tmp=[scores[i],scores[i+1],scores[i+2],scores[i+3],scores[i+4]]
-        if max(tmp)<0.6:
+        i+=1
+        if i==len(files): # marks the end of sequence
+            break
+        tmp=scores[i]
+        #  case 1
+        if tmp[0]<0.6:
             continue
-        # TODO: for cases 2 to 4
+        # case 2
+        elif tmp[1]<0.2:
+            del results[i][1]
+            del results[i][2]
+            del results[i][3]
+            del results[i][4]
+            del scores[i][1]
+            del scores[i][2]
+            del scores[i][3]
+            del scores[i][4]
+            continue
+        # case 3
+        elif tmp[1]<=0.6:
+            for j in range(2,5):
+                if tmp[j]<0.1:
+                    del results[i][j]
+                    del scores[i][j]
+            continue
+        # case 4
+        else:
+            for j in range(1,5):
+                if tmp[j]<=0.4:
+                    del results[i][j]
+                    del scores[i][j]
+            continue
 
 def showStatistic():
     global files
