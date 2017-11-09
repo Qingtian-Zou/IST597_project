b1ee294ae85613b98166b8674bb0dd80ca9ee6f3
Qingtian Zou <qingtian.mill.zou@gmail.com>
2017-11-09 09:23:21 -0500

    Finished testing confidenceProcessing. Bugs fixed.

--- output_processing.py
+++ output_processing.py
@@ -1,3 +1,8 @@
+import os
+import argparse
+
+FLAGS=None
+
 files=[]
 results=[]
 scores=[]
@@ -19,6 +24,7 @@ def loadFile(file):
         scores[int(i/5)].append(float(entry[2]))
         i+=1
     fi.close()
+    confidenceProcessing()
 
 def confidenceProcessing():
     global files
@@ -30,7 +36,6 @@ def confidenceProcessing():
     #   3. If there is only one score of a result is greater than 0.6, and not all other scores are significantly low (no greater than 0.2), then save all results whose scores are greater than 0.1.
     #   4. If multiple results are greater than 0.6, then save all results whose scores are greater than 0.4.
     # Just a reminder: the scores are already sorted from high to low when written into file.
-    i=-5
     for i in range(len(files)):
         #  case 1
         if scores[i][0]<0.6:
@@ -38,27 +43,29 @@ def confidenceProcessing():
         # case 2
         elif scores[i][1]<0.2:
             del results[i][1]
-            del results[i][2]
-            del results[i][3]
-            del results[i][4]
+            del results[i][1] # Attention: index change on del!
+            del results[i][1]
+            del results[i][1]
             del scores[i][1]
-            del scores[i][2]
-            del scores[i][3]
-            del scores[i][4]
+            del scores[i][1]
+            del scores[i][1]
+            del scores[i][1]
             continue
         # case 3
         elif scores[i][1]<=0.6:
             for j in range(2,5):
                 if scores[i][j]<0.1:
-                    del results[i][j]
-                    del scores[i][j]
+                    for _ in range(5-j):
+                        del results[i][j]
+                        del scores[i][j]
             continue
         # case 4
         else:
             for j in range(1,5):
                 if scores[i][j]<=0.4:
-                    del results[i][j]
-                    del scores[i][j]
+                    for _ in range(5-j):
+                        del results[i][j]
+                        del scores[i][j]
             continue
 
 def showStatistic():
@@ -66,3 +73,17 @@ def showStatistic():
     global results
     global scores
     # TODO: statistic results
+
+def main():
+    loadFile(FLAGS.output_file)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--output_file',
+        type=str,
+        default=os.path.join(os.getcwd(),'output.txt'),
+        help='Absolute path to the output file.'
+    )
+    FLAGS, unparsed = parser.parse_known_args()
+    main()
